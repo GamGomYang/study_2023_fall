bb8334baf61f470c6156a752b17ed37fe34b9291
GamGomYang <146828148+GamGomYang@users.noreply.github.com>
2023-10-03 23:36:01 +0900

    학생 성적표 만들기

---
+++ StudentScore.java
@@ -0,0 +1,71 @@
+package java_study;
+import java.util.Scanner;
+
+class Student{
+    String name;
+    int korean;
+    int math;
+    int english;
+
+    public Student(String name,int korean,int math,int english){
+        this.name = name;
+        this.math = math;
+        this.korean=korean;
+        this.english=english;
+    }
+
+    public String getName(){
+        return name;
+    }
+
+    public int getKorean(){
+        return korean;
+    }
+
+    public int getMath(){
+        return math;
+    }
+
+    public int getEnglish(){
+        return english;
+    }
+    public int getTotal(){
+        return english+korean+math;
+    }
+    public double getAverage(){
+        return getTotal()/3.0;
+    }
+
+}
+
+public class StudentScore {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("학생수를 입력하세요");
+        int number = scanner.nextInt();
+
+        Student[] students = new Student[number];
+
+        for (int i = 0; i < number; i++) {
+            System.out.print("이름 국어 수학 영어 순으로 작성해주세요");
+            String name = scanner.next();
+            int korean = scanner.nextInt();
+            int math = scanner.nextInt();
+            int english = scanner.nextInt();
+
+            students[i] = new Student(name, korean, math, english);
+        }
+
+        System.out.println("-성적표-");
+        System.out.println("====================");
+        System.out.println("이름    국어  수학  영어  총합  평균");
+        System.out.println("====================");
+
+        for (Student student : students) {
+            System.out.printf("%-10s %6d %6d %6d %6d %8.2f\n",
+                    student.getName(),
+                    student.getKorean(), student.getMath(), student.getEnglish(), student.getTotal(), student.getAverage());
+        }
+
+        scanner.close();
+    }}
